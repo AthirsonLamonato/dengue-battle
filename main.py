2d1ad01765c655b612903e0c10d9b1bb193aae55
Athirson <athirsonf10@gmail.com>
2023-05-04 15:56:00 -0300

    comentários

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ from Player import Jogador
 from Enemy import Inimigo
 from GoodAction import BoasAcoes
 
+
 def tela_inicial(tela, largura_tela, altura_tela, fundo_imagem):
 
     # Criação dos textos
-    titulo_texto = pygame.font.SysFont(None, 50).render("Dengue Battle", True, (255, 255, 255))
-    jogar_texto = pygame.font.SysFont(None, 30).render("Jogar", True, (255, 255, 255))
-    sair_texto = pygame.font.SysFont(None, 30).render("Sair", True, (255, 255, 255))
+    titulo_texto = pygame.font.SysFont(None, 50).render(
+        "Dengue Battle", True, (255, 255, 255))
+    jogar_texto = pygame.font.SysFont(None, 30).render(
+        "Jogar", True, (255, 255, 255))
+    sair_texto = pygame.font.SysFont(None, 30).render(
+        "Sair", True, (255, 255, 255))
 
     # Cria dois objetos Rect que serão usados para posicionar textos na tela do jogo.
-    jogar_rect = jogar_texto.get_rect(center=(largura_tela / 2, altura_tela / 2))
-    sair_rect = sair_texto.get_rect(center=(largura_tela / 2, altura_tela / 2 + 70))
+    jogar_rect = jogar_texto.get_rect(
+        center=(largura_tela / 2, altura_tela / 2))
+    sair_rect = sair_texto.get_rect(
+        center=(largura_tela / 2, altura_tela / 2 + 70))
 
     # Loop principal da tela inicial
     while True:
@@ -31,30 +37,37 @@ def tela_inicial(tela, largura_tela, altura_tela, fundo_imagem):
 
         # Desenho dos sprites
         tela.blit(fundo_imagem, (0, 0))
-        tela.blit(titulo_texto, (largura_tela / 2 - titulo_texto.get_width() / 2, 100))
+        tela.blit(titulo_texto, (largura_tela / 2 -
+                  titulo_texto.get_width() / 2, 100))
 
-        tela.blit(jogar_texto, (jogar_rect.centerx - jogar_texto.get_width() / 2, jogar_rect.centery - jogar_texto.get_height()/2))
-        tela.blit(sair_texto, (sair_rect.centerx - sair_texto.get_width() / 2, sair_rect.centery - sair_texto.get_height()/2))
+        tela.blit(jogar_texto, (jogar_rect.centerx - jogar_texto.get_width() /
+                  2, jogar_rect.centery - jogar_texto.get_height()/2))
+        tela.blit(sair_texto, (sair_rect.centerx - sair_texto.get_width() /
+                  2, sair_rect.centery - sair_texto.get_height()/2))
 
         pygame.display.update()  # Atualiza a tela
 
 def tela_game_over(tela, largura_tela, altura_tela, fundo_imagem, jogador):
-    
+
     # Desenha o fundo na tela
     tela.blit(fundo_imagem, (0, 0))
 
     # Define a fonte e as mensagens de Game Over e Pontuação Final
     fonte_game_over = pygame.font.SysFont(None, 100)
     mensagem_game_over = fonte_game_over.render("Game Over", True, (255, 0, 0))
-    mensagem_pontuacao = pygame.font.SysFont(None, 50).render(f"Pontuação: {jogador.pontos}", True, (255, 255, 255))
+    mensagem_pontuacao = pygame.font.SysFont(None, 50).render(
+        f"Pontuação: {jogador.pontos}", True, (255, 255, 255))
 
     # Define as opções de reiniciar ou sair do jogo
-    mensagem_reiniciar = pygame.font.SysFont(None, 30).render("Pressione R para reiniciar o jogo", True, (255, 255, 255))
-    mensagem_sair = pygame.font.SysFont(None, 30).render("Pressione Q para sair do jogo", True, (255, 255, 255))
+    mensagem_reiniciar = pygame.font.SysFont(None, 30).render(
+        "Pressione R para reiniciar o jogo", True, (255, 255, 255))
+    mensagem_sair = pygame.font.SysFont(None, 30).render(
+        "Pressione Q para sair do jogo", True, (255, 255, 255))
 
     # Define as posições das mensagens na tela
     mensagem_game_over_rect = mensagem_game_over.get_rect()
-    mensagem_game_over_rect.center = (largura_tela // 2, altura_tela // 2 - 100)
+    mensagem_game_over_rect.center = (
+        largura_tela // 2, altura_tela // 2 - 100)
 
     mensagem_pontuacao_rect = mensagem_pontuacao.get_rect()
     mensagem_pontuacao_rect.center = (largura_tela // 2, altura_tela // 2)
@@ -89,8 +102,6 @@ def tela_game_over(tela, largura_tela, altura_tela, fundo_imagem, jogador):
                 pygame.quit()
                 quit()
 
-
-
 # Verifica se a posição vertical do sprite é maior que a altura da tela.
 # Se for maior remove a sprite da tela
 def remover_sprites_fora_da_tela(grupo_sprites, altura_tela):
@@ -105,23 +116,23 @@ def colisao_grupo(sprite, grupo_sprites):
             return True
     return False
 
-
 # Verifica se o numero de sprites no grupo é menor que dois, se for, gera uma quantidade de sprites aleatória entre 1 e 5
 # Verifica se a sprite gerada colide com alguem do mesmo grupo, e move para outra posição
-
 def gerar_sprites_aleatorios(sprite_grupo, sprite, largura_tela):
     if len(sprite_grupo.sprites()) < 2:
         quantidade = random.randint(1, 5)
         for i in range(quantidade):
             novo_sprite = sprite.__class__()
-            novo_sprite.rect.x = random.randint(0, largura_tela - novo_sprite.rect.width)
+            novo_sprite.rect.x = random.randint(
+                0, largura_tela - novo_sprite.rect.width)
             novo_sprite.rect.y = 0
             while colisao_grupo(novo_sprite, sprite_grupo):
-                novo_sprite.rect.x = random.randint(0, largura_tela - novo_sprite.rect.width)
+                novo_sprite.rect.x = random.randint(
+                    0, largura_tela - novo_sprite.rect.width)
             sprite_grupo.add(novo_sprite)
 
 def jogo():
-    
+
     # Inicialização do Pygame
     pygame.init()
 
@@ -176,7 +187,8 @@ def jogo():
         grupo_inimigos.draw(tela)
 
         # Verifica a colisão entre o jogador e os inimigos
-        colisoes_inimigos = pygame.sprite.spritecollide(jogador, grupo_inimigos, True)
+        colisoes_inimigos = pygame.sprite.spritecollide(
+            jogador, grupo_inimigos, True)
         for inimigos in colisoes_inimigos:
             jogador.vidas -= 1
             jogador.pontos -= 1
@@ -185,12 +197,14 @@ def jogo():
                 tela_game_over(tela, largura_tela, altura_tela,fundo_imagem, jogador)
 
         # Verifica a colisão entre o jogador e as boas ações
-        colisoes_acoes = pygame.sprite.spritecollide(jogador, grupo_acoes, True)
+        colisoes_acoes = pygame.sprite.spritecollide(
+            jogador, grupo_acoes, True)
         for acao in colisoes_acoes:
             jogador.pontos += 5
 
         # Atualiza os pontos do jogador na tela
-        pontos_texto = pygame.font.SysFont(None, 30).render(f"Pontos: {jogador.pontos}", True, (255, 255, 255))
+        pontos_texto = pygame.font.SysFont(None, 30).render(
+            f"Pontos: {jogador.pontos}", True, (255, 255, 255))
         tela.blit(pontos_texto, (10, 10))
 
         # Remoção de sprites que saíram da tela
